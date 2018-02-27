665dd06c944e3abc8469c857d3b766444e39bb31
Dan Carley <dan.carley@gmail.com>
2018-02-27 18:23:55 +0000

    Log unsuccessful responses
    It's quite hard to debug problems when we can't see the response bodies.
    So log all unsuccessful responses. I've had to expose and override the
    logger to test this.

--- main.go
+++ main.go
@@ -1,25 +1,38 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-// error is always nil, otherwise API gateway won't deliver the HTTP
-// response with the correct status code and body.
+var Logger = log.New(os.Stderr, "", log.LstdFlags)
+
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var resp events.APIGatewayProxyResponse
 	switch request.HTTPMethod {
 	case http.MethodGet:
-		return CallbackHandler(request), nil
+		resp = CallbackHandler(request)
 	case http.MethodPost:
-		return UpdateHandler(request), nil
+		resp = UpdateHandler(request)
+	default:
+		resp = events.APIGatewayProxyResponse{
+			StatusCode: http.StatusMethodNotAllowed,
+			Body:       fmt.Sprintf("unsupported method: %s\n", request.HTTPMethod),
+		}
 	}
 
-	return events.APIGatewayProxyResponse{
-		StatusCode: http.StatusMethodNotAllowed,
-	}, nil
+	if resp.StatusCode >= http.StatusBadRequest {
+		Logger.Printf("Error: %d %s", resp.StatusCode, resp.Body)
+	}
+
+	// error is always nil, otherwise API gateway won't deliver the HTTP
+	// response with the correct status code and body.
+	return resp, nil
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -1,26 +1,55 @@
 package main_test
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
 
 	. "github.com/dcarley/strava-commute"
 )
 
 var _ = Describe("Handler", func() {
-	Describe("invalid request method", func() {
-		It("should return 405", func() {
-			resp, err := Handler(events.APIGatewayProxyRequest{
-				HTTPMethod: "PUT",
-			})
-			Expect(resp).To(Equal(events.APIGatewayProxyResponse{
-				StatusCode: http.StatusMethodNotAllowed,
-				Body:       ``,
-			}))
-			Expect(err).ToNot(HaveOccurred())
-		})
+	var logBuffer *gbytes.Buffer
+
+	BeforeEach(func() {
+		logBuffer = gbytes.NewBuffer()
+		Logger = log.New(logBuffer, "", 0)
 	})
+
+	DescribeTable("request methods",
+		func(req events.APIGatewayProxyRequest, expected events.APIGatewayProxyResponse, logs string) {
+			resp, err := Handler(req)
+			Expect(resp).To(Equal(expected))
+			Expect(err).ToNot(HaveOccurred())
+			Expect(string(logBuffer.Contents())).To(Equal(logs))
+		},
+		Entry("should log error response",
+			events.APIGatewayProxyRequest{
+				HTTPMethod: "PUT",
+			},
+			events.APIGatewayProxyResponse{
+				StatusCode: http.StatusMethodNotAllowed,
+				Body:       "unsupported method: PUT\n",
+			},
+			"Error: 405 unsupported method: PUT\n",
+		),
+		Entry("not log successful response",
+			events.APIGatewayProxyRequest{
+				HTTPMethod: "GET",
+				QueryStringParameters: map[string]string{
+					"hub.challenge": "foo",
+				},
+			},
+			events.APIGatewayProxyResponse{
+				StatusCode: http.StatusOK,
+				Body:       "{\"hub.challenge\":\"foo\"}\n",
+			},
+			"",
+		),
+	)
 })
